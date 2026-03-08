2bba9faa6babccaee0b3dba3315166c33ce591db
Pradyut Sarkar <pradyut.bca15@gmail.com>
2026-03-08 12:18:55 +0530

    code change

---
+++ src/main/java/com/intellifleet/PipelineTestMain.java
@@ -0,0 +1,12 @@
+package com.intellifleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PipelineTestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineTestMain.class, args);
+    }
+}
